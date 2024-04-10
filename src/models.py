a17ba12805b5e3844922b72ff1cdf8dd2e2cda3e
stanxlin <143833064+stanxlin@users.noreply.github.com>
2024-04-10 23:15:19 +0000

    Diagram of simple star wars data model

--- src/models.py
+++ src/models.py
@@ -7,23 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Users(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(Integer, nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    favorite_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    planets_id = Column(Integer, ForeignKey('planets.planet_id'), nullable=False)
+    characters_id = Column(Integer, ForeignKey('characters.characters_id'), nullable=False)
+
+    user = relationship("Users")
+    planet = relationship("Planets")
+    character = relationship("Characters")
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    planet_id = Column(Integer, primary_key=True)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    character_id = Column(Integer, primary_key=True)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    gender = Column(String(250), nullable=False)
+
+    # street_name = Column(String(250))
+    # street_number = Column(String(250))
+    # post_code = Column(String(250), nullable=False)
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    # person = relationship(Person)
 
     def to_dict(self):
         return {}
